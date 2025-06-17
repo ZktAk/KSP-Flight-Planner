26979bf75329dba9c83fa3c0f241d81c839c0897
ZktAk <zachary.theo.donaldson@gmail.com>
2025-06-16 20:23:47 -0400

    Add Antennas.py and Populate with antenna types

---
+++ src/Antennas.py
@@ -0,0 +1,81 @@
+
+class Communotron_16:
+	cost = 300                  # Kerbucks
+	mass = 0.005                # tons
+	electricity = 6             # charge/Mit
+	rating = 500 * 10^3         # m
+	packet_size = 2             # Mits
+	speed = 10/3                # Mits/s
+	relay = False
+
+class Communotron_16_S:
+	cost = 300                  # Kerbucks
+	mass = 0.015                # tons
+	electricity = 6             # charge/Mit
+	rating = 500 * 10^3         # m
+	packet_size = 2             # Mits
+	speed = 10 / 3              # Mits/s
+	relay = False
+
+class Communotron_DTS_M1:
+	cost = 900                  # Kerbucks
+	mass = 0.05                 # tons
+	electricity = 6             # charge/Mit
+	rating = 2 * 10^9           # m
+	packet_size = 2             # Mits
+	speed = 5.7142857142857     # Mits/s
+	relay = False
+
+class Communotron_HG_55:
+	cost = 1200                 # Kerbucks
+	mass = 0.075                # tons
+	electricity = 20/3          # charge/Mit
+	rating = 15 * 10^9          # m
+	packet_size = 3             # Mits
+	speed = 20                  # Mits/s
+	relay = False
+
+class Communotron_88_88:
+	cost = 1500                 # Kerbucks
+	mass = 0.1                  # tons
+	electricity = 10            # charge/Mit
+	rating = 100 * 10^9         # m
+	packet_size = 2             # Mits
+	speed = 20                  # Mits/s
+	relay = False
+
+class HG_5_High_Gain_Antenna:
+	cost = 600                  # Kerbucks
+	mass = 0.07                 # tons
+	electricity = 9             # charge/Mit
+	rating = 5 * 10^6           # m
+	packet_size = 2             # Mits
+	speed = 5.7142857142857     # Mits/s
+	relay = True
+
+class RA_2_Relay_Antenna:
+	cost = 1800                 # Kerbucks
+	mass = 0.15                 # tons
+	electricity = 24            # charge/Mit
+	rating = 2 * 10^9           # m
+	packet_size = 1             # Mits
+	speed = 2.8571428571429     # Mits/s
+	relay = True
+
+class RA_15_Relay_Antenna:
+	cost = 2400                 # Kerbucks
+	mass = 0.3                  # tons
+	electricity = 12            # charge/Mit
+	rating = 15 * 10^9          # m
+	packet_size = 2             # Mits
+	speed = 5.7142857142857     # Mits/s
+	relay = True
+
+class RA_100_Relay_Antenna:
+	cost = 3000                 # Kerbucks
+	mass = 0.65                 # tons
+	electricity = 6             # charge/Mit
+	rating = 100 * 10^9         # m
+	packet_size = 4             # Mits
+	speed = 11.428571428571     # Mits/s
+	relay = True
